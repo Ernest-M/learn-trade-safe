b578e21875d91ae5c5a053af5e13e9d35886b29b
Ernest-M <99401317+Ernest-M@users.noreply.github.com>
2026-03-13 18:43:35 +0300

    Update affiliate.ts
    Update to first working Affiliate link

--- src/config/affiliate.ts
+++ src/config/affiliate.ts
@@ -2,4 +2,4 @@
  * Central affiliate URL configuration.
  * Change this single variable to update all affiliate links site-wide.
  */
-export const AFFILIATE_XM_URL = "https://www.xm.com/?ref=YOUR_AFFILIATE_ID";
+export const AFFILIATE_XM_URL = "https://affs.click/k1w2C";
